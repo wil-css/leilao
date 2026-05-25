3dfd27ccc325bda104f88d092fffd091f024598d
Wil-css <Fox3999@hotmail.com>
2026-05-25 10:49:00 -0300

    Cadastro e listagem de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -25,14 +25,64 @@ public class ProdutosDAO {
     public void cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.execute();
+        JOptionPane.showMessageDialog(null,
+"Produto cadastrado com sucesso!");
+        prep.close();
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null,
+            "Erro ao cadastrar produto: " + e.getMessage());
+
+    }
         
         
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
-        return listagem;
+         String sql = "SELECT * FROM produtos";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while(resultset.next()){
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null,
+        "Erro ao listar produtos: " + e.getMessage());
+
+    }
+
+    return listagem;
     }
     
     
