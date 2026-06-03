9f1538cd7f327db31f0aa2591ee6826fcc54a97f
Wil-css <Fox3999@hotmail.com>
2026-06-03 00:06:36 -0300

    Implementada consulta de produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -108,11 +108,39 @@ public class ProdutosDAO {
 
         JOptionPane.showMessageDialog(null,
                 "Erro ao vender produto: " + e.getMessage());
+       }
+    }
+public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while(resultset.next()){
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null,
+                "Erro ao listar produtos vendidos: "
+                + e.getMessage());
 
     }
 
+    return listagem;
 }
-    
-        
 }
-
